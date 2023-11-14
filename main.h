f06e42a0c0bfd14191651d7c71df2c573877791c
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-14 01:01:12 -0800

    printf.c

--- main.h
+++ main.h
@@ -4,6 +4,8 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 int  print_char(char c);
+int print_string(char *s);
+int print_int(int num, int digitcount);
 int print_binary(unsigned int num, int digitcount);
 int print_unsigned_int(unsigned int num, int digitcount);
 int print_octal(unsigned int num, int digitscount);
