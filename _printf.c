c5f2688a5228e4ade9293c45c20b4214f2af02ad
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2023-11-15 00:49:35 -0800

    _printf.c

--- _printf.c
+++ _printf.c
@@ -22,7 +22,10 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (!format || format[0] == '\0' || (format[0] == '%' && !format[1]))
+	if (*format == '\0')
+		exit(-1);
+
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
